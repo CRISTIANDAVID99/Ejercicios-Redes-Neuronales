549103a1e0dae2b002cbd8647114a66332a843fe
CRISTIANDAVID99 <cristancho9921@gmail.com>
2022-06-12 21:34:18 -0500

    array mat to dataframe

--- Pseudo.py
+++ Pseudo.py
@@ -5,4 +5,9 @@ import scipy.io
 mat=scipy.io.loadmat('set1.mat')
 bdr=pd.Series(mat)
 bd=pd.DataFrame({'label':bdr.index,'list':bdr.values})
-print(bd.iloc[3,:])
+Xa=pd.DataFrame(bd.iloc[3,1])
+Xb=pd.DataFrame(bd.iloc[4,1])
+Xa[2]=1
+Xb[2]=-1
+X=pd.concat([Xa,Xb],axis=0)
+print(X)
