872425ce57d86d4f47c45e20f2d9c447ca7cd07a
CRISTIANDAVID99 <cristancho9921@gmail.com>
2022-06-12 21:11:18 -0500

    carga de base de datos

--- Pseudo.py
+++ Pseudo.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
+import scipy.io 
 
+mat=scipy.io.loadmat('set1.mat')
+bdr=pd.Series(mat)
+bd=pd.DataFrame({'label':bdr.index,'list':bdr.values})
+print(bd.iloc[3,:])
