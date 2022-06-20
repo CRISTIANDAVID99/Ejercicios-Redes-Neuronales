94c5d83f0e4fd11680c7c4099fb6510892c34137
CRISTIANDAVID99 <cristancho9921@gmail.com>
2022-06-20 17:51:37 -0500

    Wind training final

--- Clasificador_Wine.py
+++ Clasificador_Wine.py
@@ -12,7 +12,35 @@ from keras.layers.core import Dense
 
 #carga de base de datos 
 bd=pd.read_csv("wine.csv",header=None)
-print(bd)
 
-X=bd.iloc[:,1:13]
-Y=bd[0]
+X=bd.iloc[:,1:14]
+Y=bd.iloc[:,0].reset_index(drop=True)
+Y=Y.to_frame()
+ind=np.random.permutation(X.index)
+X=X.iloc[ind].reset_index(drop=True)
+Y=Y.iloc[ind].reset_index(drop=True)
+
+Xt=X.iloc[X.index<int(len(X.index)*0.8)]
+Yt=Y.iloc[Y.index<int(len(Y.index)*0.8)]
+
+keras.optimizer_v1.Adam(lr=0.1)
+model = Sequential()
+model.add(Dense(50, input_dim=13,activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error',
+              optimizer='Adam',
+              metrics=['mse'])
+
+model.fit(Xt, Yt, epochs=5000)
+
+scores = model.evaluate(Xt, Yt)
+print(scores)
+print (model.predict(Xt).round())
+Yf=pd.DataFrame(model.predict(Xt).round())
+V = (Yt.iloc[:]==Yf.iloc[:])
+
+print(V)
+freq = V.value_counts()
+print(freq)
+print(Yf)
+print(Yt)
